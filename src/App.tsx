6e66ac2e5380d1b87fae07f3303c3c8393e3564d
Moeez Ahmed <moeezahmed506@gmail..com>
2023-01-27 15:29:49 +0500

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+import "./App.css";
+// import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="text-3xl">Typescript</header>
+      <h1 className="text-3xl font-bold text-blue-400">
+        Hello world!
+      </h1>
+    </div>
+    // <Login />
+  );
+}
+
+export default App;
