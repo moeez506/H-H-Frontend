596b44323d85d8ce8d243b78415a57600ad7f24a
Ahsan Mumtaz <ahsanmumtaz008976@gmail.com>
2023-04-02 22:17:12 +0500

    Add Langiuage and Refactor the PayPal

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 // import Contact from "./components/Contact";
@@ -10,6 +10,30 @@ import RouterConfig from "./routes/RouterConfig";
 
 function App() {
   const queryClient = new QueryClient();
+  const googleTranslateElementInit = () => {
+    if (window.google.translate !== null) {
+      // eslint-disable-next-line no-new
+      new window.google.translate.TranslateElement(
+        {
+          pageLanguage: "en",
+          includedLanguages: "en,fr",
+          autoDisplay: false,
+        },
+        "google_translate_element"
+      );
+    }
+  };
+
+  useEffect(() => {
+    const addScript = document.createElement("script");
+    addScript?.setAttribute(
+      "src",
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+    );
+    document.body.appendChild(addScript);
+    window.googleTranslateElementInit = googleTranslateElementInit;
+  }, []);
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
