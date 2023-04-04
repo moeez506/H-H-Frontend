ef9500fd4bf87cff8ce322b31b585cfb040f454a
Ahsan Mumtaz <ahsanmumtaz008976@gmail.com>
2023-04-04 09:23:55 +0500

    Add PopUp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,24 @@
 /* eslint-disable prettier/prettier */
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
   return (
     <div className='bg-[#C4C7C8] h-16 mobile:hidden'>
-      <div className="absolute top-5 left-12 text-[inherit] mobile:top-2 mobile:text-sm mobile:left-2">
+      <div className='absolute top-5 left-12 text-[inherit] mobile:top-2 mobile:text-sm mobile:left-2'>
         Call Us Today! 416-519-3621|
         <a
         //   className="text-[inherit]"
         //   href="mailto:info@holdinghandscommunitynetwork.org"
         //   target="_blank"
         >
-          <span className="[text-decoration:underline]">
+          <span className='[text-decoration:underline]'>
             info@holdinghandscommunitynetwork.org
           </span>
         </a>
       </div>
-
+      <div id='google_translate_element'></div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/routes/RouterConfig.tsx
+++ src/routes/RouterConfig.tsx
@@ -1,51 +1,58 @@
 /* eslint-disable prettier/prettier */
 
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Header from "../components/Header";
-import { GroupProvider } from "../contexts/groupOnboardingContext";
-import { UserProvider } from "../contexts/individualOnboardingContext";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from '../components/Header';
+import { GroupProvider } from '../contexts/groupOnboardingContext';
+import { UserProvider } from '../contexts/individualOnboardingContext';
 import {
-    Login,
-    Register,
-    About,
-    ContactUs,
-    Home,
-    Programs,
-    MultiFormGroup,
-    MultiFormIndividual,
-    OnboardingType,
-    VerificationScreen,
-    MarketPlace,
-} from "../pages";
+  Login,
+  Register,
+  About,
+  ContactUs,
+  Home,
+  Programs,
+  MultiFormGroup,
+  MultiFormIndividual,
+  OnboardingType,
+  VerificationScreen,
+  MarketPlace,
+} from '../pages';
+import PopUp from '../components/PopUp';
 
 const RouterConfig = () => {
-    return (
-        <>
-            <Routes>
-                
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<ContactUs />} />
-                <Route path="/programs" element={<Programs />} />
-                <Route path="/market-place" element={<MarketPlace />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/onboarding-type" element={<OnboardingType />} />
-                <Route path="/verify-email/:token" element={<VerificationScreen />} />
-                <Route path="/individual-onboarding" element={
-                    <UserProvider>
-                        <MultiFormIndividual />
-                    </UserProvider>
-                } />
-                <Route path="/group-onboarding" element={
-                    <GroupProvider>
-                        <MultiFormGroup />
-                    </GroupProvider>} />
-
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<PopUp />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<ContactUs />} />
+        <Route path='/programs' element={<Programs />} />
+        <Route path='/market-place' element={<MarketPlace />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/onboarding-type' element={<OnboardingType />} />
+        <Route path='/verify-email/:token' element={<VerificationScreen />} />
+        <Route
+          path='/individual-onboarding'
+          element={
+            <UserProvider>
+              <MultiFormIndividual />
+            </UserProvider>
+          }
+        />
+        <Route
+          path='/group-onboarding'
+          element={
+            <GroupProvider>
+              <MultiFormGroup />
+            </GroupProvider>
+          }
+        />
+      </Routes>
+    </>
+  );
 };
 
 export default RouterConfig;
